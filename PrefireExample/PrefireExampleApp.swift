c05e5f7df794724f33ef8d0379f5146cb5e30b46
Arnold Noronha <arnold@screenshotbot.io>
2025-09-26 06:25:58 -0400

    Initial Commit

---
+++ PrefireExample/PrefireExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrefireExampleApp.swift
+//  PrefireExample
+//
+//  Created by Arnold Noronha on 9/26/25.
+//
+
+import SwiftUI
+
+@main
+struct PrefireExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PrefireExampleTests/PrefireExampleTests.swift
@@ -0,0 +1,17 @@
+//
+//  PrefireExampleTests.swift
+//  PrefireExampleTests
+//
+//  Created by Arnold Noronha on 9/26/25.
+//
+
+import Testing
+@testable import PrefireExample
+
+struct PrefireExampleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
